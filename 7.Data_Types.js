4bb0e94b6764f09955445c1a5f3a650b3d4d74ba
rxl-ishan-shukla <ishan.shukla@rxlogix.com>
2023-04-16 11:15:36 +0530

    Datatypes in JS

---
+++ 7.Data_Types.js
@@ -0,0 +1,20 @@
+/*
+The latest ECMAScript standard defines eight data types:
+
+Seven data types that are primitives:
+    1. Boolean. true and false.
+    2. null. A special keyword denoting a null value. (Because JavaScript is case-sensitive, 
+        null is not the same as Null, NULL, or any other variant.)
+    3. undefined. A top-level property whose value is not defined.
+    4. Number. An integer or floating point number. For example: 42 or 3.14159.
+    5. BigInt. An integer with arbitrary precision. For example: 9007199254740992n.
+    6. String. A sequence of characters that represent a text value. For example: "Howdy".
+    7. Symbol. A data type whose instances are unique and immutable.
+
+    and Object
+
+Although these data types are relatively few, they enable you to perform useful operations 
+with your applications. Functions are the other fundamental elements of the language. 
+While functions are technically a kind of object, you can think of objects as named containers 
+for values, and functions as procedures that your script can perform.
+*/
