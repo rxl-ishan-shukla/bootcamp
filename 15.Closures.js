a01e9b3ee77e214f40867f7e9a3b30aeaf0754c9
rxl-ishan-shukla <ishan.shukla@rxlogix.com>
2023-04-16 12:03:52 +0530

    Closure in JS

---
+++ 15.Closures.js
@@ -0,0 +1,8 @@
+/*
+JavaScript allows for the nesting of functions and grants the inner 
+function full access to all the variables and functions defined 
+inside the outer function. However, the outer function does not have
+access to the variables and functions defined inside the inner function.
+This provides a sort of encapsulation for the variables of the inner 
+function.
+*/
