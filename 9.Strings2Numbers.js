185d9c300334ada2f2a101c4355167f1d2285cbc
rxl-ishan-shukla <ishan.shukla@rxlogix.com>
2023-04-16 11:23:30 +0530

    strings to number conversions

---
+++ 9.Strings2Numbers.js
@@ -0,0 +1,12 @@
+// ParseInt
+let num = "1011";
+let a = parseInt(num);
+let b = parseInt(num, 2);
+
+console.log(a); // 1011
+console.log(b); // 11
+
+// ParseFloat
+let dec = "12.4";
+let c = parseFloat(dec);
+console.log(c); // 12.4
