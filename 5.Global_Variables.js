d1b4b0e45f17cbb9af43b3c57334f5f833589fe2
rxl-ishan-shukla <ishan.shukla@rxlogix.com>
2023-04-16 11:11:04 +0530

    global variables in JS

---
+++ 5.Global_Variables.js
@@ -0,0 +1,7 @@
+/*
+Global variables are in fact properties of the global object.
+In web pages, the global object is window, so you can set and 
+access global variables using the window.variable syntax.
+*/
+
+console.log(this);
